217d65d153a42ca9004e605ee1ab7a15d1ddd51a
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2023-06-02 21:56:12 +0600

    adding

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function bootstrap() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log(`database connect succesfully`)
-        // leastining port
-        app.listen(config.port, () => {
-            console.log(`Applicaation Listening Port ${config.port}`)
-        })
-    } catch (err) {
-        console.log('its an error', err)
-    }
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log(`database connect succesfully`)
+    // leastining port
+    app.listen(config.port, () => {
+      console.log(`Applicaation Listening Port ${config.port}`)
+    })
+  } catch (err) {
+    console.log('its an error', err)
+  }
 }
 
-bootstrap();
+bootstrap()
